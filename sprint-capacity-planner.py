18e2b8b8910844bece462b76eaca81a01107ec31
Robert Klajko <klajkor@gmail.com>
2020-06-09 19:49:04 +0200

    Load into dictionary implemented

--- sprint-capacity-planner.py
+++ sprint-capacity-planner.py
@@ -11,34 +11,41 @@ import credentials as cr
 start_time = time.time()
 
 
-def load_bank_holidays(file_dict, sheet_name):
-    xlsfile = file_dict['path'] + '/' + file_dict['filename']
-    print(xlsfile)
+def load_dictionary(full_file_name, sheet_name, load_max_col, load_max_row=100):
+    xlsfile = full_file_name['path'] + '/' + full_file_name['filename']
+    all_rows = []
+    if load_max_col < 1:
+        return all_rows
     try:
         wb = openpyxl.load_workbook(xlsfile, read_only=True, data_only=True)
     except:
         print("Unexpected error:", sys.exc_info()[0])
-    else:
-        load_max_col = 3
-        keys = []
-        all_rows = []
+        return all_rows
+    keys = []
+    try:
         sheet = wb[sheet_name]
-        for firstrow in sheet.iter_rows(min_row=1, max_row=1, min_col=0, max_col=load_max_col, values_only=True):
-            for key in firstrow:
-                keys.append(key)
-        for row in sheet.iter_rows(min_row=2, min_col=0, max_col=load_max_col, values_only=True):
-            onerow = {}
-            for i in range(0, load_max_col):
-                onerow[firstrow[i]] = row[i]
-            all_rows.append(onerow)
-        print(all_rows)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        return all_rows
+    for firstrow in sheet.iter_rows(min_row=1, max_row=1, min_col=0, max_col=load_max_col, values_only=True):
+        for key in firstrow:
+            keys.append(key)
+    for row in sheet.iter_rows(min_row=2, max_row=load_max_row, min_col=0, max_col=load_max_col, values_only=True):
+        onerow = {}
+        for i in range(0, load_max_col):
+            onerow[firstrow[i]] = row[i]
+        all_rows.append(onerow)
+    return all_rows
 
 
 if __name__ == '__main__':
     now = datetime.datetime.now()
     print("Started at:", now.strftime("%Y-%m-%d %H:%M:%S"))
     print("#####----------------------------------#####")
-    load_bank_holidays(cr.testexcelfile, 'Bank holidays')
+    print(load_dictionary(cr.testexcelfile, 'Bank holidays', 3))
+    print(load_dictionary(cr.testexcelfile, 'Vacations', 16, 200))
+    print(load_dictionary(cr.testexcelfile, 'Developers', 5))
+    print(load_dictionary(cr.testexcelfile, 'Sprints', 7))
     print("#####----------------------------------#####")
     print("--- Execution time: %s seconds ---" % (time.time() - start_time))
 
