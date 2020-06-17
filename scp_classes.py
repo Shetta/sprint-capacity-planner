7eb34b2c46861a0e906b57cbc7bbd847da879c52
Robert Klajko <klajkor@gmail.com>
2020-06-18 00:31:29 +0200

    data processing functions

--- scp_classes.py
+++ scp_classes.py
@@ -1,24 +1,29 @@
 # Sprint Capacity Planner class definitions
 import datetime
-from dataclasses import astuple, dataclass
+from dataclasses import dataclass, field
+#from typing import List
+import pandas as pd
 
 
 @dataclass
 class Default:
-    Name: str
+    name: str
     Value: int
 
 
 @dataclass
 class BankHoliday:
-    Country: str
-    Date: datetime.datetime
-    Comment: str
+    dates: list  # list of dicts: {'date': current_date_date, 'countries': countries[] }
+
+    def is_holiday(self, datetime_date):
+        if isinstance(datetime_date, datetime.datetime):
+            datetime_date = datetime_date.date()
+        return next((item for item in self.dates if item['date'] == datetime_date), None)
 
 
 @dataclass
 class Developer:
-    Name: str
+    name: str
     Country: str
     FTE: float
     Start_date_on_project: datetime.datetime
@@ -46,6 +51,35 @@ class Vacation:
 
 
 @dataclass
+class EmployeeVacation:
+    name: str
+    vacation_days: list
+
+    def add_vacations_from_range(self, range_start, range_end):
+        date_range = pd.bdate_range(range_start, range_end)
+        for single_date in date_range:
+            self.vacation_days.append(single_date.date())
+        self.vacation_days.sort()
+
+    def add_vacation_date(self, single_date):
+        if isinstance(single_date, datetime.datetime):
+            single_date = single_date.date()
+        self.vacation_days.append(single_date)
+        self.vacation_days.sort()
+
+    def print_all_vacations(self):
+        print(self.name)
+        for vacation in self.vacation_days:
+            print(vacation)
+
+    def is_on_holiday(self, daytime_date):
+        if daytime_date in self.vacation_days:
+            return True
+        else:
+            return False
+
+
+@dataclass
 class Sprint:
     Sprint: str
     Start_date: datetime.datetime
@@ -55,4 +89,37 @@ class Sprint:
     Dev_team_size_HU: float
     Dev_team_size_total: float
 
+    def define_workday_dates(self):
+        date_range = pd.bdate_range(self.Start_date, self.End_date)
+        for single_date in date_range:
+            print(single_date.strftime("%Y-%m-%d"))
+
+
+@dataclass
+class SprintDetails:
+    sprint: str
+    start_date: datetime.datetime
+    end_date: datetime.datetime
+    dev_team_size_UK: float
+    dev_team_size_HU: float
+    dev_team_size_total: float = 0.0
+    workdays_in_sprint: int = 0
+    capacity: float = 0.0
+    members_on_holiday: list = field(init=False, repr=False)
+    headcount_on_holiday: list = field(init=False, repr=False)
+
+    def __post_init__(self):
+        self.dev_team_size_total = self.dev_team_size_HU + self.dev_team_size_UK
+        self.workdays_in_sprint = len(pd.bdate_range(start=self.start_date, end=self.end_date))
+        self.members_on_holiday = []
+        self.headcount_on_holiday = []
+
+    def get_dev_team_size_total(self):
+        self.dev_team_size_total = self.dev_team_size_HU + self.dev_team_size_UK
+        return self.dev_team_size_total
+
+    def add_employee_on_holiday(self, date, name):
+        if isinstance(date, pd.Timestamp):
+            date = date.date()
+        self.members_on_holiday.append({'date': date, 'name': name})
 
--- sprint-capacity-planner.py
+++ sprint-capacity-planner.py
@@ -4,22 +4,29 @@ import time
 import openpyxl
 import sys
 from dataclasses import fields
+import pandas as pd
 
 # local include files
 import credentials as cr
-from scp_classes import Default, BankHoliday, Developer, Vacation, Sprint
+from scp_classes import Default, BankHoliday, Developer, Sprint, EmployeeVacation, SprintDetails
 
 # global variables
-bank_holidays = []
-vacations = []
-developers = []
-sprints = []
-defaults = []
+vacations_list = []
+bank_holidays_list = []
+developers_list = []
+sprints_list = []
+defaults_list = []
+vacations_obj = []
+bank_holidays_obj = []
+developers_obj = []
+sprints_obj = []
+defaults_obj = []
+employee_vacations_list = []
 
 start_time = time.time()
 
 
-def load_dictionary(full_file_name, sheet_name, load_max_col, load_max_row=50):
+def load_to_dictionary(full_file_name, sheet_name, load_max_col, load_max_row=50):
     xlsfile = full_file_name['path'] + '/' + full_file_name['filename']
     all_rows = []
     if load_max_col < 1:
@@ -30,23 +37,25 @@ def load_dictionary(full_file_name, sheet_name, load_max_col, load_max_row=50):
         return all_rows
     try:
         wb = openpyxl.load_workbook(xlsfile, read_only=True, data_only=True)
-    except:
+    except Exception:
         print("Unexpected error:", sys.exc_info()[0])
         return all_rows
     keys = []
     try:
         sheet = wb[sheet_name]
-    except:
+    except Exception:
         print("Unexpected error:", sys.exc_info()[0])
         return all_rows
-    for firstrow in sheet.iter_rows(min_row=1, max_row=1, min_col=0, max_col=load_max_col, values_only=True):
-        for key in firstrow:
+    for first_row in sheet.iter_rows(min_row=1, max_row=1, min_col=0, max_col=load_max_col, values_only=True):
+        for key in first_row:
             keys.append(key)
     for row in sheet.iter_rows(min_row=2, max_row=load_max_row, min_col=0, max_col=load_max_col, values_only=True):
-        onerow = {}
+        if row[0] is None:
+            continue
+        one_row = {}
         for i in range(0, load_max_col):
-            onerow[firstrow[i]] = row[i]
-        all_rows.append(onerow)
+            one_row[first_row[i]] = row[i]
+        all_rows.append(one_row)
     return all_rows
 
 
@@ -82,39 +91,130 @@ def load_to_objects(object_class, full_file_name, sheet_name, load_max_col, load
     return all_rows
 
 
-def load_all():
-    global bank_holidays
-    global vacations
-    global developers
-    global sprints
-    global defaults
-    bank_holidays = load_dictionary(cr.testexcelfile, 'Bank holidays', 3, 100)
-    vacations = load_dictionary(cr.testexcelfile, 'Vacations', 16, 200)
-    developers = load_dictionary(cr.testexcelfile, 'Developers', 5)
-    sprints = load_dictionary(cr.testexcelfile, 'Sprints', 7, 100)
-    defaults = load_dictionary(cr.testexcelfile, 'Defaults', 2)
+def vacations_data_process(list_of_vacations, list_of_bank_holidays, list_of_employees):
+    list_of_employee_vacations = []
+    list_of_vacations = sorted(list_of_vacations, key=lambda x: (x['EMPLOYEE'], x['START DATE']))
+    list_of_bank_holidays = sorted(list_of_bank_holidays, key=lambda x: (x['Date']))
+    list_of_employees = sorted(list_of_employees, key=lambda x: (x['Name']))
+    for employee in list_of_employees:
+        employee_bank_holidays = get_employee_bank_holidays(employee['Name'], list_of_employees, list_of_bank_holidays)
+        employee_obj = EmployeeVacation(employee['Name'], employee_bank_holidays)
+        filtered_vacations = [vac for vac in list_of_vacations if vac['EMPLOYEE'] == employee['Name']]
+        for vacation_record in filtered_vacations:
+            start_date_date = vacation_record['START DATE']
+            if isinstance(start_date_date, datetime.datetime):
+                start_date_date = start_date_date.date()
+            end_date_date = vacation_record['END DATE']
+            if isinstance(end_date_date, datetime.datetime):
+                end_date_date = end_date_date.date()
+            employee_obj.add_vacations_from_range(start_date_date, end_date_date)
+        list_of_employee_vacations.append(employee_obj)
+    return list_of_employee_vacations
+
+
+def bank_holidays_data_process(list_of_bank_holidays):
+    all_holidays = []
+    countries = []
+    list_of_bank_holidays = sorted(list_of_bank_holidays, key=lambda x: (x['Date']))
+    current_date = ""
+    current_date_date = ""
+    for item in list_of_bank_holidays:
+        if item['Date'] != current_date:
+            if current_date != "":
+                all_holidays.append({'date': current_date_date, 'countries': countries})
+                countries = []
+            current_date = item['Date']
+            if isinstance(current_date, datetime.datetime):
+                current_date_date = current_date.date()
+            else:
+                current_date_date = current_date
+        countries.append(item['Country'])
+    all_holidays.append({'date': current_date_date, 'countries': countries})
+    holidays_obj = BankHoliday(all_holidays)
+    return holidays_obj
+
+
+def who_is_on_holiday(datetime_date):
+    on_holiday = []
+    global employee_vacations_list
+    for employee in employee_vacations_list:
+        if employee.is_on_holiday(datetime_date):
+            on_holiday.append(employee.name)
+    return on_holiday
+
+
+def get_employee_bank_holidays(employee, list_of_employees, list_of_bank_holidays):
+    employee_bank_holidays = []
+    employee_dict = next((item for item in list_of_employees if item['Name'] == employee), None)
+    if type(employee_dict) == dict:
+        country_code = employee_dict['Country']
+        for bank_holiday in list_of_bank_holidays:
+            if bank_holiday['Country'] == country_code:
+                employee_bank_holidays.append(bank_holiday['Date'].date())
+
+    return employee_bank_holidays
+
+
+def load_all_to_dict():
+    global bank_holidays_list
+    global vacations_list
+    global developers_list
+    global sprints_list
+    global defaults_list
+    bank_holidays_list = load_to_dictionary(cr.testexcelfile, 'Bank holidays', 3, 100)
+    vacations_list = load_to_dictionary(cr.testexcelfile, 'Vacations', 16, 200)
+    developers_list = load_to_dictionary(cr.testexcelfile, 'Developers', 5)
+    sprints_list = load_to_dictionary(cr.testexcelfile, 'Sprints', 7, 100)
+    defaults_list = load_to_dictionary(cr.testexcelfile, 'Defaults', 2)
 
 
 def load_all_to_object():
-    global bank_holidays
-    global vacations
-    global developers
-    global sprints
-    global defaults
-    bank_holidays = load_to_objects(BankHoliday, cr.testexcelfile, 'Bank holidays', 3, 100)
-    vacations = load_to_objects(Vacation, cr.testexcelfile, 'Vacations', 16, 200)
-    developers = load_to_objects(Developer, cr.testexcelfile, 'Developers', 5)
-    sprints = load_to_objects(Sprint, cr.testexcelfile, 'Sprints', 7, 100)
-    defaults = load_to_objects(Default, cr.testexcelfile, 'Defaults', 2)
+    global developers_obj
+    global sprints_obj
+    global defaults_obj
+    developers_obj = load_to_objects(Developer, cr.testexcelfile, 'Developers', 5)
+    sprints_obj = load_to_objects(Sprint, cr.testexcelfile, 'Sprints', 7, 100)
+    defaults_obj = load_to_objects(Default, cr.testexcelfile, 'Defaults', 2)
+
+
+def sprints_data_process(list_of_sprints, list_of_employee_vacations):
+    list_of_sprint_details = []
+    for sprint in list_of_sprints:
+        sprint_details_obj = SprintDetails(sprint['Sprint'], sprint['Start date'], sprint['End date'],
+                                           sprint['Dev team size UK'], sprint['Dev team size HU'])
+        date_range = pd.bdate_range(sprint['Start date'], sprint['End date'])
+        for single_date in date_range:
+            for vacation_obj in list_of_employee_vacations:
+                if vacation_obj.is_on_holiday(single_date):
+                    sprint_details_obj.add_employee_on_holiday(single_date, vacation_obj.name)
+        list_of_sprint_details.append(sprint_details_obj)
+    return list_of_sprint_details
 
 
 if __name__ == '__main__':
     now = datetime.datetime.now()
     print("Started at:", now.strftime("%Y-%m-%d %H:%M:%S"))
     print("#####----------------------------------#####")
+    load_all_to_dict()
+    employee_vacations_list = vacations_data_process(vacations_list, bank_holidays_list, developers_list)
+    # for employee_obj in employee_vacations_list:
+    #    employee_obj.print_all_vacations()
+    bank_holidays_obj = bank_holidays_data_process(bank_holidays_list)
+    sprint_details_list = sprints_data_process(sprints_list, employee_vacations_list)
+    for sprint_detail in sprint_details_list:
+        print(sprint_detail)
+        print(sprint_detail.members_on_holiday)
+    #print(get_employee_bank_holidays('Garra Peters', developers_list, bank_holidays_list))
+    #print(employee_vacations_list)
+    x_date = datetime.date(datetime.date.today().year, 8, 18)
+    #print(x_date)
+    #print(who_is_on_holiday(x_date))
 
-    load_all_to_object()
+    #print(bank_holidays_obj)
+    #print(bank_holidays_obj.is_holiday(datetime.date(2020, 12, 25)))
+    #load_all_to_object()
 
+    #sp1 = sprints[0]
+    #sp1.define_workday_dates()
     print("#####----------------------------------#####")
     print("--- Execution time: %s seconds ---" % (time.time() - start_time))
-
